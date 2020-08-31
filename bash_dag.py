eb9450f089ef6b15ad5eebb11f0deaffc0b8fc26
workflow-test <70399684+workflow-test@users.noreply.github.com>
2020-08-31 20:40:55 +0530

    Update bash_dag.py

--- bash_dag.py
+++ bash_dag.py
@@ -36,7 +36,7 @@ run_this >> run_this_last
 for i in range(3):
     task = BashOperator(
         task_id='runme_' + str(i),
-        bash_command='echo "{{ task_instance_key_str }}" && sleep 1',
+        bash_command='echo "{{ task_instance_key_str }}" && sleep 10',
         dag=dag,
     )
     task >> run_this
